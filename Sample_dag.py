048daa8b7c11ff826cf2e6b0f057ba28eebd9222
rajputrishabh <rishabhrajput30@gmail.com>
2024-08-10 14:15:41 +0530

    Update Sample_dag.py

--- Sample_dag.py
+++ Sample_dag.py
@@ -1,35 +1,21 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
-def print_hello():
-    return 'Hello world!'
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2024, 8, 8),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
-dag = DAG(
-    'simple_test_dag',
-    default_args=default_args,
-    description='A simple test DAG'
-    schedule_interval=timedelta(days=1),
-)
-start = DummyOperator(
-    task_id='start',
+
+dag = DAG('hello_world', default_args=default_args, schedule_interval=timedelta(days=1))
+
+t1 = BashOperator(
+    task_id='say_hello',
+    bash_command='echo "Hello World from Airflow!"',
     dag=dag,
 )
-hello_task = PythonOperator(
-    task_id='hello_task',
-    python_callable=print_hello,
-    dag=dag,
-)
-end = DummyOperator(
-    task_id='end',
-    dag=dag,
-)
-start >> hello_task >> end
